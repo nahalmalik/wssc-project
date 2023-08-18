7d21c5da7053e1b12e49415cf9e4d94ec5e834b7
Shayan Khan <shayanm1215225@gmail.com>
2023-08-19 00:27:17 +0500

    jfj

--- API/insert-api.php
+++ API/insert-api.php
@@ -5,8 +5,23 @@ header ('Access-Control-Allow-Methods: POST');
 header ('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include('../links/conn.php');
 $data = json_decode(file_get_contents ("php://input"), true);
-$Img=$_FILES['img'];
-print_r($Img);
+// $file=$_FILES['img'];
+$addres=$data['addres'];
+$detale=$data['detal'];
+$filename=$_FILES['img']['name'];
+$filepath=$_FILES['img']['tmp_name'];
+$fileerror=$_FILES['img']['error'];
+if($fileerror==0){
+$destfile = "../assets/img/".$filename;
+move_uploaded_file($filepath,$destfile);
+}
+// print_r($addres);
+$insert = mysqli_query($conn,"INSERT INTO `clintcomp`( `Image`, `Address`, `Detale`, `Type`) VALUES ('$filename','$addres','$detale','Drainage')");
+if ($insert){
+    echo json_encode(array('message' => 'Student Record Inserted.', 'status' => true));
+}else{
+    echo json_encode(array('message' => 'Student Record Not Inserted.', 'status' => false));
+}
 
 
 ?>
